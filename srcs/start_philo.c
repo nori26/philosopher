94a4c0fd826b3a1cd1bd03f366a67aa2d30c5761
nori <nori623p@gmail.com>
2021-08-22 17:47:40 +0200

    done

--- srcs/start_philo.c
+++ srcs/start_philo.c
@@ -19,11 +19,6 @@ void	*start_philo(void *arg)
 	return (NULL);
 }
 
-int64_t	num_of_phi(t_data *data)
-{
-	return (mtx_do_func(&data->phi->num_of_phi, &data->mtxnum, ret_arg));
-}
-
 t_thinkfunc	select_thinking(t_data *data)
 {
 	return ((t_thinkfunc[2]){think, think_odd}[data->phi->num_of_phi % 2]);
