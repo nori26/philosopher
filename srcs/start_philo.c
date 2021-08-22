0e7a3e6e758ced5a45dc9592f5be60144f330e1f
nori <nori623p@gmail.com>
2021-08-22 22:48:22 +0200

    mand done

--- srcs/start_philo.c
+++ srcs/start_philo.c
@@ -4,6 +4,8 @@ void	*start_philo(void *arg)
 {
 	if (((t_data *)arg)->phi->num_of_phi == 1)
 		return (NULL);
+	if (!(((t_data *)arg)->num % 2))
+		usleep(1000);
 	forks_init(arg);
 	while (is_living(arg) && is_hungry(arg))
 	{
