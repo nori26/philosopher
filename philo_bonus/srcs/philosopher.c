a1fadec3cee30b4edd28daabb4eaa45aa07941b5
nori <nori623p@gmail.com>
2021-09-01 08:55:21 +0200

    philo tmp

--- philo_bonus/srcs/philosopher.c
+++ philo_bonus/srcs/philosopher.c
@@ -1,7 +1,19 @@
 #include "philo_bonus.h"
 
 void	*philosopher(void *arg)
-{}
+{
+	delay_if_even(arg);
+	forks_init(arg);
+	while (continue_simulation(arg))
+	{
+		get_forks(arg);
+		eating(arg);
+		release_forks(arg);
+		sleeping(arg);
+		thinking(arg);
+	}
+	return (NULL);
+}
 
 void	*phisolopher(void *arg)
 {
