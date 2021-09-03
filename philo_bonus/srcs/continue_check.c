4b482eaab7b2c15843d821eaed84934f8a598f19
nori <nori623p@gmail.com>
2021-09-03 11:17:02 +0200

    test

--- philo_bonus/srcs/continue_check.c
+++ philo_bonus/srcs/continue_check.c
@@ -6,7 +6,7 @@
 /*   By: nosuzuki <nosuzuki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/03 00:01:27 by nosuzuki          #+#    #+#             */
-/*   Updated: 2021/09/03 11:11:52 by nosuzuki         ###   ########.fr       */
+/*   Updated: 2021/09/03 11:16:58 by nosuzuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,18 @@
 
 int64_t	continue_simulation(t_phi *philo)
 {
+	printf("%lld wait out lock\n", philo->num);
 	return (sem_do_func(philo, philo->outer, continue_inner));
 }
 
 int64_t	continue_inner(t_phi *philo)
 {
-	// printf("%lld in out lock\n", philo->num);
+	printf("%lld in out lock\n", philo->num);
 	return (sem_do_func(philo, philo->inner, continue_check));
 }
 
 int64_t	continue_check(t_phi *philo)
 {
-	// printf("%lld in inner lock\n", philo->num);
+	printf("%lld in inner lock\n", philo->num);
 	return (!philo->end);
 }
