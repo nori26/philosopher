f9c0b40ba07a260d8c901688be440923e7bdec47
nori <nori623p@gmail.com>
2021-09-03 11:21:07 +0200

    test

--- philo_bonus/srcs/continue_check.c
+++ philo_bonus/srcs/continue_check.c
@@ -6,7 +6,7 @@
 /*   By: nosuzuki <nosuzuki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/03 00:01:27 by nosuzuki          #+#    #+#             */
-/*   Updated: 2021/09/03 11:20:28 by nosuzuki         ###   ########.fr       */
+/*   Updated: 2021/09/03 11:21:02 by nosuzuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int64_t	continue_simulation(t_phi *philo)
 int64_t	continue_inner(t_phi *philo)
 {
 	printf("%lld in out lock\n", philo->num);
-	return (sem_do_func(philo, philo->inner, continue_check) | !printf("%lld unlock outer\n", philo->num));
+	return (sem_do_func(philo, philo->inner, continue_check) | !printf("%lld unlock inner\n", philo->num));
 	// return (sem_do_func(philo, philo->inner, continue_check));
 }
 
