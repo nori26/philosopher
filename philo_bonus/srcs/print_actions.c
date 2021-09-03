f1371fc1fe4488d8a9433a634abae94519372fa4
nori <nori623p@gmail.com>
2021-09-03 10:37:05 +0200

    test

--- philo_bonus/srcs/print_actions.c
+++ philo_bonus/srcs/print_actions.c
@@ -6,7 +6,7 @@
 /*   By: nosuzuki <nosuzuki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/03 00:01:27 by nosuzuki          #+#    #+#             */
-/*   Updated: 2021/09/03 00:01:27 by nosuzuki         ###   ########.fr       */
+/*   Updated: 2021/09/03 10:37:00 by nosuzuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,22 @@
 
 void	actions(t_phi *philo, int action, int64_t sleeptime)
 {
-	if (print_status(&(t_print){philo, action}))
+	// if (print_status(&(t_print){philo, action}))
+	if (print_status(&(t_print){philo, action}) | (action == EAT ? !printf("%lld back outer\n", philo->num) : 0))
 		return ;
 	mymsleep(sleeptime, philo);
 }
 
 int64_t	print_status(t_print *p)
 {
-	return (sem_do_func(p, p->philo->outer, inner_lock));
+	// return (sem_do_func(p, p->philo->outer, inner_lock));
+	return (sem_do_func(p, p->philo->outer, inner_lock)  | (p->action == EAT ? !printf("%lld back inner\n", p->philo->num) : 0));
 }
 
 int64_t	inner_lock(t_print *p)
 {
-	return (sem_do_func(p, p->philo->inner, print));
-}
+	// return (sem_do_func(p, p->philo->inner, print));
+	return (sem_do_func(p, p->philo->inner, print) | (p->action == EAT ? !printf("%lld back print\n", p->philo->num) : 0));
 
 int64_t	print(t_print *p)
 {
